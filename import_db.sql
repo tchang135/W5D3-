b36cebd919e7ef3f9dddb91e217d201f5567e861
Timothy Chang <tchang135@gmail.com>
2022-12-07 12:09:14 -0500

    insert into values for question follows

--- import_db.sql
+++ import_db.sql
@@ -17,23 +17,47 @@ FOREIGN KEY (associated_author_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY, 
     user_id INTEGER,
     question_id INTEGER
 
+FOREIGN KEY (user_id) REFERENCES users(id)
+FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
-    subject INTEGER NOT NULL,
-    parent INTEGER,
-    body TEXT NOT NULL;
-    user_id INTEGER not NULL;
+    question_id INTEGER NOT NULL,
+    parent_id INTEGER,
+    body TEXT NOT NULL,
+    user_id INTEGER not NULL
 
-FOREIGN KEY (parent) REFERENCES (id)
+FOREIGN KEY (parent_id) REFERENCES (id)
+FOREIGN KEY (user_id) REFERENCES users(id)
+FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 CREATE TABLE question_like (
     user_id INTEGER NOT NULL,
-    question_id INTEGER NOT NULL,
-    likes INTEGER
+    question_id INTEGER NOT NULL
+
+FOREIGN KEY (user_id) REFERENCES users(id)
+FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO 
+    users(fname, lname)
+VALUES 
+    ('Timothy', 'Chang'),
+    ('Wilson', 'Wu');
+
+
+INSERT INTO 
+    questions(title, body, associated_author_id)
+VALUES 
+    ('What is Big O?', 'Im having trouble understanding the concept of Big O, could someone explain it to me?', 1)
+    ('What is Recursion', 'Im stuck on the rescursion HW, could someone help me with question 5?', 2)
+    
+
+INSERT INTO 
+    question_follows()
