7691b91212b44c869f87a15e144ed2d164faf185
Wilson Wu <wuyecheng738@gmail.com>
2022-12-07 11:35:03 -0500

    finished create table

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY, 
     fname TEXT NOT NULL, 
@@ -15,7 +17,23 @@ FOREIGN KEY (associated_author_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
-    id INTEGER PRIMARY KEY,
-    
+    user_id INTEGER,
+    question_id INTEGER
 
-)
+);
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    subject INTEGER NOT NULL,
+    parent INTEGER,
+    body TEXT NOT NULL;
+    user_id INTEGER not NULL;
+
+FOREIGN KEY (parent) REFERENCES (id)
+);
+
+CREATE TABLE question_like (
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    likes INTEGER
+);
