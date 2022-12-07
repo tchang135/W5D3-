bded954d8fe3bb9d86946bac7de63cf7539a1395
Timothy Chang <tchang135@gmail.com>
2022-12-07 11:09:16 -0500

    writing question follows

---
+++ import_db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY, 
+    fname TEXT NOT NULL, 
+    lname TEXT NOT NULL 
+
+); 
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY, 
+    title TEXT NOT NULL, 
+    body TEXT NOT NULL, 
+    associated_author_id INTEGER NOT NULL  
+
+FOREIGN KEY (associated_author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    
+
+)
